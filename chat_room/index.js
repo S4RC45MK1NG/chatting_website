fd52797eb206ea2ec0a152ef3d8c60b0d393a432
SarcasmKing <sarcasmking12@gmail.com>
2026-06-28 22:29:57 +0530

    t13

--- chat_room/index.js
+++ chat_room/index.js
@@ -100,7 +100,7 @@ mediaInput.addEventListener("change", () => {
     console.log("sent media to server");
 
     const files = mediaInput.files;
-    socket.emit(files[0]);
+    socket.emit("media", files[0]);
 })
 
 // Disconnect
